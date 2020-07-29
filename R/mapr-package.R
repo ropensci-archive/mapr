d832bacce67a1d35427dbe08979cb0fc924d1020
Scott Chamberlain <myrmecocystus@gmail.com>
2020-07-29 10:25:15 -0700

    pkg level man file needed description

--- R/mapr-package.R
+++ R/mapr-package.R
@@ -1,29 +1,26 @@
-#' Visualize species occurrence data
-#'
+#' @title mapr
+#' @description Visualize species occurrence data
 #' @section Many inputs:
 #' All functions take the following kinds of inputs:
-#' \itemize{
-#'  \item An object of class `occdat`, from the package \pkg{spocc}.
+#' 
+#' - An object of class `occdat`, from the package \pkg{spocc}.
 #'  An object of this class is composed of many objects of class
 #'  `occdatind`
-#'  \item An object of class `occdatind`, from the package \pkg{spocc}
-#'  \item An object of class `gbif`, from the package \pkg{rgbif}
-#'  \item An object of class `data.frame`. This data.frame can have any
+#' - An object of class `occdatind`, from the package \pkg{spocc}
+#' - An object of class `gbif`, from the package \pkg{rgbif}
+#' - An object of class `data.frame`. This data.frame can have any
 #'  columns, but must include a column for taxonomic names (e.g., `name`),
 #'  and for latitude and longitude (we guess your lat/long columns, starting
 #'  with the default `latitude` and `longitude`)
-#'  \item An object of class `SpatialPoints`
-#'  \item An object of class `SpatialPointsDatFrame`
-#' }
+#' - An object of class `SpatialPoints`
+#' - An object of class `SpatialPointsDatFrame`
 #'
 #' @section Package API:
-#' \itemize{
-#'  \item [map_plot()] - static Base R plots
-#'  \item [map_ggplot()] - static ggplot2 plots
-#'  \item [map_ggmap()] - static ggplot2 plots with map layers
-#'  \item [map_leaflet()] - interactive Leaflet.js interactive maps
-#'  \item [map_gist()] - ineractive, shareable maps on GitHub Gists
-#' }
+#' 
+#' - [map_plot()] - static Base R plots
+#' - [map_ggplot()] - static ggplot2 plots
+#' - [map_leaflet()] - interactive Leaflet.js interactive maps
+#' - [map_gist()] - ineractive, shareable maps on GitHub Gists
 #'
 #' @importFrom ggplot2 geom_point aes ggtitle labs map_data
 #' ggplot geom_point geom_polygon element_blank theme scale_color_manual
